815de2816c3ad7a2808bd87f19ed1ca86ae023da
Taiga Noumi <Noumi.Taiga@gmail.com>
2012-09-04 23:46:33 +0900

    add float, double compare

--- picotest.h
+++ picotest.h
@@ -33,7 +33,7 @@
 #include <cassert>
 #include <sstream>
 #include <algorithm>
-
+#include <stdint.h>
 // workaround
 #if defined _WIN32
 #define PICOTEST_WINDOWS
@@ -53,22 +53,170 @@ struct Registry;
 struct Registrar;
 
 namespace detail {
+	// print with color
 	enum Color {
 		COLOR_RED,
 		COLOR_GREEN
 	};
-	TestCase* getCurrentTestCase();
-	Test* getCurrentTest();
-	void setCurrentTestCase(TestCase* testcase);
-	void setCurrentTest(Test* test);
-	void coloredPrint(Color c, const char* fmt, ...);
 
+#ifdef PICOTEST_WINDOWS
+	WORD getColorAttr(Color c) {
+		switch (c) {
+		case COLOR_RED:    return FOREGROUND_RED;
+		case COLOR_GREEN:  return FOREGROUND_GREEN;
+		default:           assert(0); return 0;
+		}
+	}
+#endif
+
+	void coloredPrint(Color c, const char* fmt, ...) {
+		va_list args;
+		va_start(args, fmt);
+
+#ifdef PICOTEST_WINDOWS
+		const HANDLE std_handle = ::GetStdHandle(STD_OUTPUT_HANDLE);
+
+		CONSOLE_SCREEN_BUFFER_INFO buffer_info;
+		::GetConsoleScreenBufferInfo(std_handle, &buffer_info);
+		const WORD old_color = buffer_info.wAttributes;
+		const WORD new_color  = getColorAttr(c) | FOREGROUND_INTENSITY;
+
+		fflush(stdout);
+		::SetConsoleTextAttribute(std_handle, new_color);
+
+		vprintf(fmt, args);
+
+		fflush(stdout);
+		::SetConsoleTextAttribute(std_handle, old_color);
+#else
+		vprintf(fmt, args);
+#endif
+		va_end(args);
+	}
+
+	// fallback operator <<
 	template <typename Char, typename CharTraits, typename T>
 	::std::basic_ostream<Char, CharTraits>& operator<<(
 		::std::basic_ostream<Char, CharTraits>& os, const T& v) {
-		os << "(" << sizeof(v) << "-byte object)";
-		return os;
+			os << "(" << sizeof(v) << "-byte object)";
+			return os;
 	}
+
+	// compare floating number using ULP
+	struct Floating {
+		static const size_t MIN_UPS = 4;
+
+		union float_ {
+			float value;
+			uint32_t raw;
+		};
+
+		union double_ {
+			double value;
+			uint64_t raw;
+		};
+
+		static uint32_t sam (uint32_t bits) {
+			return bits & 0x80000000 ? ~bits + 1 : bits | 0x80000000;
+		}
+
+		static uint64_t sam (uint64_t bits) {
+			return bits & 0x8000000000000000LL ? ~bits + 1 : bits | 0x8000000000000000LL;
+		}
+
+		template<typename T>
+		static T distance (const T& v1, const T& v2) {
+			const T sam1 = sam(v1), sam2 = sam(v2);
+			return sam1 >= sam2 ? (sam1 - sam2) : (sam2 - sam1);
+		}
+
+		static bool almostEqual(float v1, float v2) {
+			float_ v1_, v2_;
+			v1_.value = v1;
+			v2_.value = v2;
+			return distance(v1_.raw, v2_.raw) <= MIN_UPS;
+		}
+
+		static bool almostEqual(double v1, double v2) {
+			double_ v1_, v2_;
+			v1_.value = v1;
+			v2_.value = v2;
+			return distance(v1_.raw, v2_.raw) <= MIN_UPS;
+		}
+	};
+
+	// binary operators
+	struct LT {
+		template <class T1, class T2>
+		bool operator()(const T1& v1, const T2& v2) { return v1 < v2; }
+		static std::string toStr() { return "<"; }
+	};
+
+	struct GT {
+		template <class T1, class T2>
+		bool operator()(const T1& v1, const T2& v2) { return v1 > v2; }
+		static std::string toStr() { return ">"; }
+	};
+
+	struct LE {
+		template <class T1, class T2>
+		bool operator()(const T1& v1, const T2& v2) { return v1 <= v2; }
+		static std::string toStr() { return "<="; }
+	};
+
+	struct GE {
+		template <class T1, class T2>
+		bool operator()(const T1& v1, const T2& v2) { return v1 >= v2; }
+		static std::string toStr() { return ">="; }
+	};
+
+	struct EQ {
+		template <class T1, class T2>
+		bool operator()(const T1& v1, const T2& v2) { return v1 == v2; }
+		static std::string toStr() { return "=="; }
+	};
+
+	struct NE {
+		template <class T1, class T2>
+		bool operator()(const T1& v1, const T2& v2) { return v1 == v2; }
+		static std::string toStr() { return "!="; }
+	};
+
+	struct STREQ {
+		bool operator()(const char* v1, const char* v2) { return strcmp(v1, v2) == 0; }
+		static std::string toStr() { return "=="; }
+	};
+
+	struct STRNE {
+		bool operator()(const char* v1, const char* v2) { return strcmp(v1, v2) != 0; }
+		static std::string toStr() { return "!="; }
+	};
+
+	struct STRCASEEQ {
+		bool operator()(const char* v1, const char* v2) { return _stricmp(v1, v2) == 0; }
+		static std::string toStr() { return "=="; }
+	};
+
+	struct STRCASENE {
+		bool operator()(const char* v1, const char* v2) { return _stricmp(v1, v2) != 0; }
+		static std::string toStr() { return "!="; }
+	};
+
+	struct FLOATEQ {
+		template<typename T>
+		bool operator()(const T& v1, const T& v2) {
+			return Floating::almostEqual(v1, v2);
+		}
+		static std::string toStr() { return "=="; }
+	};
+
+	struct FLOATNE {
+		template<typename T>
+		bool operator()(const T& v1, const T& v2) {	
+			return !FLOATEQ()(v1, v2);
+		}
+		static std::string toStr() { return "!="; }
+	};
 }
 
 /////////////////////////////////////////////////////////////////
@@ -80,6 +228,23 @@ struct TestState {
 		static TestState instance;
 		return instance;
 	}
+
+	static TestCase* getCurrentTestCase() {
+		return getInstance().testcase_;
+	}
+
+	static Test* getCurrentTest() {
+		return getInstance().test_;
+	}
+
+	static void setCurrentTestCase(TestCase* testcase) {
+		getInstance().testcase_ = testcase;
+	}
+
+	static void setCurrentTest(Test* test) {
+		getInstance().test_ = test;
+	}
+
 	TestCase* testcase_;
 	Test* test_;
 };
@@ -101,7 +266,7 @@ public:
 	Test (const std::string& name, TestFunc f) : executed_(false), name_(name), f_(f) {}
 
 	void execute() {
-		detail::setCurrentTest(this);
+		TestState::setCurrentTest(this);
 		f_();
 		executed_ = true;
 	}
@@ -143,7 +308,7 @@ public:
 	}
 
 	void execute() {
-		detail::setCurrentTestCase(this);
+		TestState::setCurrentTestCase(this);
 		
 		std::for_each(tests_.begin(), tests_.end(), std::mem_fun_ref(&Test::execute));
 
@@ -247,25 +412,9 @@ struct Registrar {
 
 namespace detail {
 
-	TestCase* getCurrentTestCase() {
-		return TestState::getInstance().testcase_;
-	}
-
-	Test* getCurrentTest() {
-		return TestState::getInstance().test_;
-	}
-
-	void setCurrentTestCase(TestCase* testcase) {
-		TestState::getInstance().testcase_ = testcase;
-	}
-
-	void setCurrentTest(Test* test) {
-		TestState::getInstance().test_ = test;
-	}
-
 	bool assertBool(bool expected, bool result, const std::string& cond, const std::string& file, int line) {
 		if (expected != result) {
-			Test* current = getCurrentTest();
+			Test* current = TestState::getCurrentTest();
 			assert(current);
 			current->setFailure(file, line, cond, result ? "true" : "false");
 		}
@@ -278,144 +427,12 @@ namespace detail {
 		bool c = op(expected, result);
 
 		if (!c) {
-			Test* current = getCurrentTest();
+			Test* current = TestState::getCurrentTest();
 			assert(current);
-			current->setFailure(file, line, cond, ::foo::condStr(expected, result, op));
+			current->setFailure(file, line, cond, ::picotest_detail::condStr(expected, result, op));
 		}
 		return c;
 	}
-
-
-#ifdef PICOTEST_WINDOWS
-	WORD getColorAttr(Color c) {
-		switch (c) {
-		case COLOR_RED:    return FOREGROUND_RED;
-		case COLOR_GREEN:  return FOREGROUND_GREEN;
-		default:           assert(0); return 0;
-		}
-	}
-#endif
-
-	void coloredPrint(Color c, const char* fmt, ...) {
-		va_list args;
-		va_start(args, fmt);
-
-#ifdef PICOTEST_WINDOWS
-		const HANDLE std_handle = ::GetStdHandle(STD_OUTPUT_HANDLE);
-
-		CONSOLE_SCREEN_BUFFER_INFO buffer_info;
-		::GetConsoleScreenBufferInfo(std_handle, &buffer_info);
-		const WORD old_color = buffer_info.wAttributes;
-		const WORD new_color  = getColorAttr(c) | FOREGROUND_INTENSITY;
-
-		fflush(stdout);
-		::SetConsoleTextAttribute(std_handle, new_color);
-
-		vprintf(fmt, args);
-
-		fflush(stdout);
-		::SetConsoleTextAttribute(std_handle, old_color);
-#else
-		vprintf(fmt, args);
-#endif
-		va_end(args);
-	}
-
-	struct yes {};
-	struct no {};
-	
-	template<bool B>
-	struct is_true {
-		typedef yes type;
-	};
-
-	template<>
-	struct is_true<false>{
-		typedef no type;
-	};
-
-	template<typename T>
-	struct has_insertion_op {
-		typedef char yes;
-		typedef struct { char a[2]; } no;
-		template<typename U> static yes& test(U&);
-		template<typename U> static no& test(...);
-		static std::ostream& s;
-		static const T& t;
-		static const bool value = sizeof(test(s << t)) == sizeof(yes);
-	};
-
-	/*template<bool B, class T = void>
-	struct enable_if_c {
-		typedef T type;
-	};
-
-	template<class T>
-	struct enable_if_c<false, T> {};
-
-	template<typename Cond, class T = void>
-	struct enable_if : public enable_if_c<Cond::value, T> {};*/
-
-
-
-
-
-	// binary operators
-	struct LT {
-		template <class T1, class T2>
-		bool operator()(const T1& v1, const T2& v2) { return v1 < v2; }
-		static std::string toStr() { return "<"; }
-	};
-
-	struct GT {
-		template <class T1, class T2>
-		bool operator()(const T1& v1, const T2& v2) { return v1 > v2; }
-		static std::string toStr() { return ">"; }
-	};
-
-	struct LE {
-		template <class T1, class T2>
-		bool operator()(const T1& v1, const T2& v2) { return v1 <= v2; }
-		static std::string toStr() { return "<="; }
-	};
-
-	struct GE {
-		template <class T1, class T2>
-		bool operator()(const T1& v1, const T2& v2) { return v1 >= v2; }
-		static std::string toStr() { return ">="; }
-	};
-
-	struct EQ {
-		template <class T1, class T2>
-		bool operator()(const T1& v1, const T2& v2) { return v1 == v2; }
-		static std::string toStr() { return "=="; }
-	};
-
-	struct NE {
-		template <class T1, class T2>
-		bool operator()(const T1& v1, const T2& v2) { return v1 == v2; }
-		static std::string toStr() { return "!="; }
-	};
-
-	struct STREQ {
-		bool operator()(const char* v1, const char* v2) { return strcmp(v1, v2) == 0; }
-		static std::string toStr() { return "=="; }
-	};
-
-	struct STRNE {
-		bool operator()(const char* v1, const char* v2) { return strcmp(v1, v2) != 0; }
-		static std::string toStr() { return "!="; }
-	};
-
-	struct STRCASEEQ {
-		bool operator()(const char* v1, const char* v2) { return _stricmp(v1, v2) == 0; }
-		static std::string toStr() { return "=="; }
-	};
-
-	struct STRCASENE {
-		bool operator()(const char* v1, const char* v2) { return _stricmp(v1, v2) != 0; }
-		static std::string toStr() { return "!="; }
-	};
 
 } // namespace detail
 
@@ -440,7 +457,7 @@ protected:
 
 } // namespace testing
 
-namespace foo {
+namespace picotest_detail {
 	template<typename T1, typename T2, typename OP>
 	std::string condStr(const T1& v1, const T2& v2, OP op){
 		std::ostringstream os;
@@ -495,8 +512,10 @@ void PICOTEST_IDENITY(test_case_name, test_name)::test_method()
 /////////////////////////////////////////////////////////////////
 // EXPECT_XX
 
-#define EXPECT_BOOL(expected, actual) picotest::detail::assertBool(expected, actual, foo::condStr(#expected, #actual, picotest::detail::EQ()), __FILE__, __LINE__)
-#define EXPECT_BINARY(left, right, OP) picotest::detail::assertBinary(left, right, PICOTEST_JOIN(picotest::detail::, OP()), foo::condStr(#left, #right, picotest::detail::OP()), __FILE__, __LINE__)
+#define EXPECT_BOOL(expected, actual) \
+	picotest::detail::assertBool(expected, actual, picotest_detail::condStr(#expected, #actual, picotest::detail::EQ()), __FILE__, __LINE__)
+#define EXPECT_BINARY(left, right, OP) \
+	picotest::detail::assertBinary(left, right, PICOTEST_JOIN(picotest::detail::, OP()), picotest_detail::condStr(#left, #right, picotest::detail::OP()), __FILE__, __LINE__)
 
 #define EXPECT_TRUE(cond) EXPECT_BOOL(true, cond)
 #define EXPECT_FALSE(cond) EXPECT_BOOL(false, cond)
@@ -509,6 +528,10 @@ void PICOTEST_IDENITY(test_case_name, test_name)::test_method()
 #define EXPECT_STRNE(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, STRNE)
 #define EXPECT_STRCASEEQ(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, STRCASEEQ)
 #define EXPECT_STRCASENE(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, STRCASENE)
+#define EXPECT_FLOAT_EQ(expected, actual)  EXPECT_BINARY(expected, actual, FLOATEQ)
+#define EXPECT_DOUBLE_EQ(expected, actual) EXPECT_BINARY(expected, actual, FLOATEQ)
+#define EXPECT_FLOAT_NE(expected, actual)  EXPECT_BINARY(expected, actual, FLOATNE)
+#define EXPECT_DOUBLE_NE(expected, actual) EXPECT_BINARY(expected, actual, FLOATNE)
 
 /////////////////////////////////////////////////////////////////
 // ASSERT_XX
@@ -538,6 +561,10 @@ do {\
 #define ASSERT_STRNE(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, STRNE)
 #define ASSERT_STRCASEEQ(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, STRCASEEQ)
 #define ASSERT_STRCASENE(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, STRCASENE)
+#define ASSERT_FLOAT_EQ(expected, actual)  ASSERT_BINARY(expected, actual, FLOATEQ)
+#define ASSERT_DOUBLE_EQ(expected, actual) ASSERT_BINARY(expected, actual, FLOATEQ)
+#define ASSERT_FLOAT_NE(expected, actual)  ASSERT_BINARY(expected, actual, FLOATNE)
+#define ASSERT_DOUBLE_NE(expected, actual) ASSERT_BINARY(expected, actual, FLOATNE)
 
 /////////////////////////////////////////////////////////////////
 // RUNNING ALL TESTS
