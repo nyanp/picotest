4fb5760362bf0928ff9c6df195b83863d912aac8
Taiga Noumi <Noumi.Taiga@gmail.com>
2012-09-10 00:55:25 +0900

    refactoring
    move binary operator functors from picotest::detail to picotest

--- picotest.h
+++ picotest.h
@@ -203,96 +203,7 @@ namespace detail {
 #endif
 	}
 
-	/***** binary operators *****/
 
-	struct LT {
-		template <typename T1, typename T2>
-		bool operator()(const T1& lhs, const T2& rhs) { return lhs < rhs; }
-		static std::string name() { return "<"; }
-	};
-
-	struct GT {
-		template <typename T1, typename T2>
-		bool operator()(const T1& lhs, const T2& rhs) { return lhs > rhs; }
-		static std::string name() { return ">"; }
-	};
-
-	struct LE {
-		template <typename T1, typename T2>
-		bool operator()(const T1& lhs, const T2& rhs) { return lhs <= rhs; }
-		static std::string name() { return "<="; }
-	};
-
-	struct GE {
-		template <typename T1, typename T2>
-		bool operator()(const T1& lhs, const T2& rhs) { return lhs >= rhs; }
-		static std::string name() { return ">="; }
-	};
-
-	struct EQ {
-		template <typename T1, typename T2>
-		bool operator()(const T1& lhs, const T2& rhs) { return lhs == rhs; }
-
-		// use when comparing against null
-		template <typename T>
-		bool operator()(int lhs, T* const rhs) {
-			return reinterpret_cast<const int*>(lhs) == rhs;
-		}
-
-		template <typename T>
-		bool operator()(T* const lhs, int rhs) {
-			return lhs == reinterpret_cast<const int*>(rhs);
-		}
-
-		static std::string name() { return "=="; }
-	};
-
-	struct NE {
-		template <class T1, class T2>
-		bool operator()(const T1& lhs, const T2& rhs) { return !EQ()(lhs, rhs); }//lhs == rhs; }
-		static std::string name() { return "!="; }
-	};
-
-	struct STREQ {
-		bool operator()(const char* lhs, const char* rhs) { return strcmp(lhs, rhs) == 0; }
-		static std::string name() { return "=="; }
-	};
-
-	struct STRNE {
-		bool operator()(const char* lhs, const char* rhs) { return strcmp(lhs, rhs) != 0; }
-		static std::string name() { return "!="; }
-	};
-
-	struct STRCASEEQ {
-		bool operator()(const char* lhs, const char* rhs) { 
-			return stricmp(lhs, rhs) == 0; 
-		}
-		static std::string name() { return "=="; }
-	};
-
-	struct STRCASENE {
-		bool operator()(const char* lhs, const char* rhs) { 
-			return stricmp(lhs, rhs) != 0; 
-		
-		}
-		static std::string name() { return "!="; }
-	};
-
-	struct FLOATEQ {
-		template<typename T>
-		bool operator()(const T& lhs, const T& rhs) {
-			return Floating::almostEqual(lhs, rhs);
-		}
-		static std::string name() { return "=="; }
-	};
-
-	struct FLOATNE {
-		template<typename T>
-		bool operator()(const T& lhs, const T& rhs) {	
-			return !FLOATEQ()(lhs, rhs);
-		}
-		static std::string name() { return "!="; }
-	};
 }
 
 /////////////////////////////////////////////////////////////////
@@ -348,6 +259,7 @@ struct Failure {
 
 class Test {
 public:
+	typedef std::vector<Failure> Failures;
 	typedef void (*TestFunc)(void);
 
 	Test (const std::string& name, TestFunc f) : executed_(false), name_(name), f_(f) {}
@@ -371,7 +283,7 @@ public:
 	}
 
 	void reportFailure(std::ostream& os) const {
-		for (std::vector<Failure>::const_iterator it = failures_.begin(), end = failures_.end(); it != end; ++it) 
+		for (Failures::const_iterator it = failures_.begin(), end = failures_.end(); it != end; ++it) 
 			report(os, (*it));
 	}
 private:
@@ -380,13 +292,15 @@ private:
 	}
 
 	bool executed_;
-	std::vector<Failure> failures_;
+	Failures failures_;
 	std::string name_;
 	TestFunc f_;
 };
 
 class TestCase {
 public:
+	typedef std::vector<Test> Tests;
+
 	TestCase() {}
 	TestCase(const std::string& name) : executed_(false), name_(name) {}
 
@@ -406,14 +320,17 @@ public:
 		coloredPrint(detail::COLOR_RED, "[ FAILED ] ");
 		os << name_ << std::endl;
 
-		for (std::vector<Test>::const_iterator it = tests_.begin(), end = tests_.end(); it != end; ++it)
+		for (Tests::const_iterator it = tests_.begin(), end = tests_.end(); it != end; ++it)
 			if (!(*it).success()) (*it).reportFailure(os);
 	}
 
 	bool success() const {
 		if (!executed_) return false;
 
-		if (std::find_if(tests_.begin(), tests_.end(), std::not1(std::mem_fun_ref(&Test::success))) == tests_.end())
+		if (std::find_if(
+				tests_.begin(), 
+				tests_.end(), 
+				std::not1(std::mem_fun_ref(&Test::success))) == tests_.end())
 			return true;
 
 		return false; // at least one test failed
@@ -426,18 +343,20 @@ public:
 private:
 	bool executed_;
 	std::string name_;
-	std::vector<Test> tests_;
+	Tests tests_;
 };
 
 struct Registry {
 public:
+	typedef std::vector<TestCase> TestCases;
+
 	static Registry& getInstance() {
 		static Registry instance;
 		return instance;
 	}
 
 	void add(const std::string& test_case_name, const Test& t) {
-		std::vector<TestCase>::iterator found = find_by_name(test_case_name);
+		TestCases::iterator found = find_by_name(test_case_name);
 
 		if (found == tests_.end()) {
 			tests_.push_back(TestCase(test_case_name));
@@ -456,7 +375,7 @@ public:
 		if (failed) {
 			os << numFailed() << " of " << numTotal() << " tests failed." << std::endl;
 
-			for (std::vector<TestCase>::const_iterator it = tests_.begin(), end = tests_.end(); it != end; ++it)
+			for (TestCases::const_iterator it = tests_.begin(), end = tests_.end(); it != end; ++it)
 				if (!(*it).success()) (*it).report(os);
 		} else {
 			os << numSuccess() << "tests success." << std::endl;
@@ -484,14 +403,14 @@ private:
 
 	PICOTEST_DISALLOW_COPY_AND_ASSIGN(Registry);
 
-	std::vector<TestCase>::iterator find_by_name(const std::string& test_case_name) {
-		for (std::vector<TestCase>::iterator it = tests_.begin(), end = tests_.end(); it != end; ++it)
+	TestCases::iterator find_by_name(const std::string& test_case_name) {
+		for (TestCases::iterator it = tests_.begin(), end = tests_.end(); it != end; ++it)
 			if ((*it).name() == test_case_name) return it;
 
 		return tests_.end();
 	}
 
-	std::vector<TestCase> tests_;
+	TestCases tests_;
 };
 
 struct Registrar {
@@ -502,15 +421,109 @@ struct Registrar {
 
 } // namespace picotest::framework
 
+/***** binary operators *****/
+
+struct LT {
+	template <typename T1, typename T2>
+	bool operator()(const T1& lhs, const T2& rhs) { return lhs < rhs; }
+	static std::string name() { return "<"; }
+};
+
+struct GT {
+	template <typename T1, typename T2>
+	bool operator()(const T1& lhs, const T2& rhs) { return lhs > rhs; }
+	static std::string name() { return ">"; }
+};
+
+struct LE {
+	template <typename T1, typename T2>
+	bool operator()(const T1& lhs, const T2& rhs) { return lhs <= rhs; }
+	static std::string name() { return "<="; }
+};
+
+struct GE {
+	template <typename T1, typename T2>
+	bool operator()(const T1& lhs, const T2& rhs) { return lhs >= rhs; }
+	static std::string name() { return ">="; }
+};
+
+struct EQ {
+	template <typename T1, typename T2>
+	bool operator()(const T1& lhs, const T2& rhs) { return lhs == rhs; }
+
+	// use when comparing against null
+	template <typename T>
+	bool operator()(int lhs, T* const rhs) {
+		return reinterpret_cast<const int*>(lhs) == rhs;
+	}
+
+	template <typename T>
+	bool operator()(T* const lhs, int rhs) {
+		return lhs == reinterpret_cast<const int*>(rhs);
+	}
+
+	static std::string name() { return "=="; }
+};
+
+struct NE {
+	template <class T1, class T2>
+	bool operator()(const T1& lhs, const T2& rhs) { return !EQ()(lhs, rhs); }
+	static std::string name() { return "!="; }
+};
+
+struct STREQ {
+	bool operator()(const char* lhs, const char* rhs) { return strcmp(lhs, rhs) == 0; }
+	static std::string name() { return "=="; }
+};
+
+struct STRNE {
+	bool operator()(const char* lhs, const char* rhs) { return strcmp(lhs, rhs) != 0; }
+	static std::string name() { return "!="; }
+};
+
+struct STRCASEEQ {
+	bool operator()(const char* lhs, const char* rhs) { 
+		return detail::stricmp(lhs, rhs) == 0; 
+	}
+	static std::string name() { return "=="; }
+};
+
+struct STRCASENE {
+	bool operator()(const char* lhs, const char* rhs) { 
+		return detail::stricmp(lhs, rhs) != 0; 
+
+	}
+	static std::string name() { return "!="; }
+};
+
+struct FLOATEQ {
+	template<typename T>
+	bool operator()(const T& lhs, const T& rhs) {
+		return detail::Floating::almostEqual(lhs, rhs);
+	}
+	static std::string name() { return "=="; }
+};
+
+struct FLOATNE {
+	template<typename T>
+	bool operator()(const T& lhs, const T& rhs) {	
+		return !FLOATEQ()(lhs, rhs);
+	}
+	static std::string name() { return "!="; }
+};
+
+/***** compare implementation *****/
+
 template<typename T1, typename T2, typename OP>
-bool compare(const T1& expected, const T2& actual, OP op, const char* expected_str, const char* actual_str, const char* file, int line) {
+bool compare(const T1& expected, const T2& actual, OP op, 
+			 const char* expected_str, const char* actual_str, const char* file, int line) {
 	bool test_success = op(expected, actual);
 
 	if (!test_success) {
 		framework::TestState::getCurrentTest()->setFailure(
 			framework::Failure(file, line,
-			makeExpressionStr(expected_str, actual_str, op),
-			makeExpressionStr(expected, actual, op)));		
+			detail::makeExpressionStr(expected_str, actual_str, op),
+			detail::makeExpressionStr(expected, actual, op)));		
 	}
 	return test_success;
 }
@@ -519,7 +532,8 @@ inline bool evaluate(bool expected, bool actual, const char* expression, const c
 	bool test_success = expected == actual;
 
 	if (!test_success) {
-		framework::TestState::getCurrentTest()->setFailure(framework::Failure(file, line, expression, expected));	
+		framework::TestState::getCurrentTest()->setFailure(
+			framework::Failure(file, line, expression, expected));	
 	}
 
 	return test_success;
@@ -552,9 +566,15 @@ protected:
 // test with auto-registration
 
 #define PICOTEST_IDENITY(test_case_name, test_name) PICOTEST_JOIN(test_case_name, test_name)
-#define PICOTEST_TEST_CASE_INVOKER(test_case_name, test_name) PICOTEST_JOIN(PICOTEST_IDENITY(test_case_name, test_name), _invoker)
-#define PICOTEST_TEST_CASE_REGISTRAR(test_case_name, test_name) static picotest::framework::Registrar PICOTEST_JOIN(PICOTEST_IDENITY(test_case_name, test_name), _registrar)
-#define PICOTEST_MAKE_TEST(test_case_name, test_name) picotest::framework::Test(PICOTEST_STR(test_name), PICOTEST_TEST_CASE_INVOKER(test_case_name, test_name))
+
+#define PICOTEST_TEST_CASE_INVOKER(test_case_name, test_name) \
+PICOTEST_JOIN(PICOTEST_IDENITY(test_case_name, test_name), _invoker)
+
+#define PICOTEST_TEST_CASE_REGISTRAR(test_case_name, test_name) \
+static picotest::framework::Registrar PICOTEST_JOIN(PICOTEST_IDENITY(test_case_name, test_name), _registrar)
+
+#define PICOTEST_MAKE_TEST(test_case_name, test_name) \
+picotest::framework::Test(PICOTEST_STR(test_name), PICOTEST_TEST_CASE_INVOKER(test_case_name, test_name))
 
 #define TEST(test_case_name, test_name) \
 PICOTEST_TEST_CASE_AUTO_REGISTER(test_case_name, test_name, ::testing::Test)
@@ -591,20 +611,20 @@ void PICOTEST_IDENITY(test_case_name, test_name)::test_method()
 
 #define EXPECT_TRUE(cond) EXPECT_BOOL(true, cond)
 #define EXPECT_FALSE(cond) EXPECT_BOOL(false, cond)
-#define EXPECT_EQ(expected, actual) EXPECT_BINARY(expected, actual, picotest::detail::EQ)
-#define EXPECT_NE(expected, actual) EXPECT_BINARY(expected, actual, picotest::detail::NE)
-#define EXPECT_LT(expected, actual) EXPECT_BINARY(expected, actual, picotest::detail::LT)
-#define EXPECT_GT(expected, actual) EXPECT_BINARY(expected, actual, picotest::detail::GT)
-#define EXPECT_LE(expected, actual) EXPECT_BINARY(expected, actual, picotest::detail::LE)
-#define EXPECT_GE(expected, actual) EXPECT_BINARY(expected, actual, picotest::detail::GE)
-#define EXPECT_STREQ(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, picotest::detail::STREQ)
-#define EXPECT_STRNE(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, picotest::detail::STRNE)
-#define EXPECT_STRCASEEQ(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, picotest::detail::STRCASEEQ)
-#define EXPECT_STRCASENE(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, picotest::detail::STRCASENE)
-#define EXPECT_FLOAT_EQ(expected, actual)  EXPECT_BINARY(expected, actual, picotest::detail::FLOATEQ)
-#define EXPECT_DOUBLE_EQ(expected, actual) EXPECT_BINARY(expected, actual, picotest::detail::FLOATEQ)
-#define EXPECT_FLOAT_NE(expected, actual)  EXPECT_BINARY(expected, actual, picotest::detail::FLOATNE)
-#define EXPECT_DOUBLE_NE(expected, actual) EXPECT_BINARY(expected, actual, picotest::detail::FLOATNE)
+#define EXPECT_EQ(expected, actual) EXPECT_BINARY(expected, actual, picotest::EQ)
+#define EXPECT_NE(expected, actual) EXPECT_BINARY(expected, actual, picotest::NE)
+#define EXPECT_LT(expected, actual) EXPECT_BINARY(expected, actual, picotest::LT)
+#define EXPECT_GT(expected, actual) EXPECT_BINARY(expected, actual, picotest::GT)
+#define EXPECT_LE(expected, actual) EXPECT_BINARY(expected, actual, picotest::LE)
+#define EXPECT_GE(expected, actual) EXPECT_BINARY(expected, actual, picotest::GE)
+#define EXPECT_STREQ(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, picotest::STREQ)
+#define EXPECT_STRNE(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, picotest::STRNE)
+#define EXPECT_STRCASEEQ(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, picotest::STRCASEEQ)
+#define EXPECT_STRCASENE(expected_str, actual_str) EXPECT_BINARY(expected_str, actual_str, picotest::STRCASENE)
+#define EXPECT_FLOAT_EQ(expected, actual)  EXPECT_BINARY(expected, actual, picotest::FLOATEQ)
+#define EXPECT_DOUBLE_EQ(expected, actual) EXPECT_BINARY(expected, actual, picotest::FLOATEQ)
+#define EXPECT_FLOAT_NE(expected, actual)  EXPECT_BINARY(expected, actual, picotest::FLOATNE)
+#define EXPECT_DOUBLE_NE(expected, actual) EXPECT_BINARY(expected, actual, picotest::FLOATNE)
 
 /////////////////////////////////////////////////////////////////
 // ASSERT_XX
@@ -625,20 +645,20 @@ do {\
 
 #define ASSERT_TRUE(cond) ASSERT_BOOL(true, cond)
 #define ASSERT_FALSE(cond) ASSERT_BOOL(false, cond)
-#define ASSERT_EQ(expected, actual) ASSERT_BINARY(expected, actual, picotest::detail::EQ)
-#define ASSERT_NE(expected, actual) ASSERT_BINARY(expected, actual, picotest::detail::NE)
-#define ASSERT_LT(expected, actual) ASSERT_BINARY(expected, actual, picotest::detail::LT)
-#define ASSERT_GT(expected, actual) ASSERT_BINARY(expected, actual, picotest::detail::GT)
-#define ASSERT_LE(expected, actual) ASSERT_BINARY(expected, actual, picotest::detail::LE)
-#define ASSERT_GE(expected, actual) ASSERT_BINARY(expected, actual, picotest::detail::GE)
-#define ASSERT_STREQ(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, picotest::detail::STREQ)
-#define ASSERT_STRNE(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, picotest::detail::STRNE)
-#define ASSERT_STRCASEEQ(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, picotest::detail::STRCASEEQ)
-#define ASSERT_STRCASENE(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, picotest::detail::STRCASENE)
-#define ASSERT_FLOAT_EQ(expected, actual)  ASSERT_BINARY(expected, actual, picotest::detail::FLOATEQ)
-#define ASSERT_DOUBLE_EQ(expected, actual) ASSERT_BINARY(expected, actual, picotest::detail::FLOATEQ)
-#define ASSERT_FLOAT_NE(expected, actual)  ASSERT_BINARY(expected, actual, picotest::detail::FLOATNE)
-#define ASSERT_DOUBLE_NE(expected, actual) ASSERT_BINARY(expected, actual, picotest::detail::FLOATNE)
+#define ASSERT_EQ(expected, actual) ASSERT_BINARY(expected, actual, picotest::EQ)
+#define ASSERT_NE(expected, actual) ASSERT_BINARY(expected, actual, picotest::NE)
+#define ASSERT_LT(expected, actual) ASSERT_BINARY(expected, actual, picotest::LT)
+#define ASSERT_GT(expected, actual) ASSERT_BINARY(expected, actual, picotest::GT)
+#define ASSERT_LE(expected, actual) ASSERT_BINARY(expected, actual, picotest::LE)
+#define ASSERT_GE(expected, actual) ASSERT_BINARY(expected, actual, picotest::GE)
+#define ASSERT_STREQ(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, picotest::STREQ)
+#define ASSERT_STRNE(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, picotest::STRNE)
+#define ASSERT_STRCASEEQ(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, picotest::STRCASEEQ)
+#define ASSERT_STRCASENE(expected_str, actual_str) ASSERT_BINARY(expected_str, actual_str, picotest::STRCASENE)
+#define ASSERT_FLOAT_EQ(expected, actual)  ASSERT_BINARY(expected, actual, picotest::FLOATEQ)
+#define ASSERT_DOUBLE_EQ(expected, actual) ASSERT_BINARY(expected, actual, picotest::FLOATEQ)
+#define ASSERT_FLOAT_NE(expected, actual)  ASSERT_BINARY(expected, actual, picotest::FLOATNE)
+#define ASSERT_DOUBLE_NE(expected, actual) ASSERT_BINARY(expected, actual, picotest::FLOATNE)
 
 /////////////////////////////////////////////////////////////////
 // RUNNING ALL TESTS
